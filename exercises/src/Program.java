eefbb0ecb6c69f164bfa033a521c7ad20ec929dc
ShaiOren4761 <shaioren100@gmail.com>
2025-01-30 23:18:42 +0200

    test
    Holy fuck this sucks

---
+++ exercises/src/Program.java
@@ -0,0 +1,103 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		// a + b
+		int a = 5;
+		int b = 10;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		
+		int c = a + b;
+		
+		System.out.println("c = " + c);
+		
+		// b - a
+		a = 5;
+		b = 10;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		
+		c = b - a;
+		
+		System.out.println("c = " + c);
+		
+		// a * b
+		a = 5;
+		b = 10;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		
+		c = a * b;
+		
+		System.out.println("c = " + c);
+		
+		// b / a
+		a = 5;
+		b = 10;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		
+		c = b / a;
+		
+		System.out.println("c = " + c);
+		
+		// b % a
+		a = 5;
+		b = 10;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		
+		c = b % a;
+		
+		System.out.println("c = " + c);
+		
+		// a / b
+		a = 5;
+		b = 10;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		
+		c = a / b;
+		
+		System.out.println("c = " + c);
+		
+		// a % b
+		a = 5;
+		b = 10;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		
+		c = a % b;
+		
+		System.out.println("c = " + c);
+		
+
+		// a / b in a float
+		a = 5;
+		b = 10;
+		
+		System.out.println("a = " + a + ", b = " + b);
+		
+		float f = a / b;
+		
+		System.out.println("f = " + f);
+		
+		// a1 / b1
+		float a1 = 5;
+		float b1 = 10;
+		
+		System.out.println("a1 = " + a1 + ", b1 = " + b1);
+		
+		float c1 = a1 / b1;
+		
+		System.out.println("c1 = " + c1);
+				
+		// 4.9
+		
+		
+		
+	}
+
+}
